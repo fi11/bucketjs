3b058ee80c96d3f90e34b8e2b5d7d91e47fc62f5
fi11 <pavelsilinw@gmail.com>
2014-11-08 11:11:28 +0300

    fix path to dist for export src

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
+var path = require('path');
 
-exports.src = fs.readFileSync('./dist/bucket.min.js').toString();
+exports.src = fs.readFileSync(path.join(__dirname, 'dist/bucket.min.js')).toString();
 
 exports.require = function(resource, options, fn) {
     return "bucket.require('" +
-        resource + "', " + JSON.stringify(options) + ", "
+        resource + "'," + JSON.stringify(options) + ","
         + (fn ? fn.toString() : 'undefined') +
         ");";
 };
