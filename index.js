d28e941758411e2b17b96fe261536bf95a256862
fi11 <pavelsilinw@gmail.com>
2014-11-30 19:00:02 +0300

    remove src from index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-var fs = require('fs');
-var path = require('path');
-
-exports.src = fs.readFileSync(path.join(__dirname, 'dist/bucket.min.js')).toString();
-
 exports.require = function(resource, options, fn) {
     return "bucket.require('" +
         resource + "'," + JSON.stringify(options) + ","
