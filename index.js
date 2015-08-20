d7d7f61e6cf43eea0529f81c5fc828980f9f309d
fi11 <pavelsilinw@gmail.com>
2015-08-20 19:19:10 +0300

    add exporting module path for nodejs

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-exports.require = function(resource, options, fn) {
-    return "bucket.require('" +
-        resource + "'," + JSON.stringify(options) + ","
-        + (fn ? fn.toString() : 'undefined') +
-        ");";
+module.exports = {
+    require: function(resource, options, fn) {
+        return "bucket.require('" +
+            resource + "'," + JSON.stringify(options) + ","
+            + (fn ? fn.toString() : 'undefined') +
+            ");";
+    },
+    PATH: typeof __dirname === "undefined" ? null : __dirname
 };
